42e0a4397802be59397493d7172c9c173df76c47
MelvinMihaylov <melvinmihaylov@gmail.com>
2023-09-13 15:34:00 +0300

    Eloquent relationships added to Answer and Question models

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Answer extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['answer', 'question_id'];
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
 }
--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['question'];
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
 }
